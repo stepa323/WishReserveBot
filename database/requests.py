b2801756595bf49c66630ad960b3c4ae548dc7c3
stepan <voytov.st.vi@gmail.com>
2025-08-10 16:16:34 +0200

    added func get_or_create_user

--- database/requests.py
+++ database/requests.py
@@ -1,1 +1,16 @@
+from sqlalchemy import select, update, delete, desc
+
+from database.models import User, async_session
+
+async def get_or_create_user(telegram_id):
+    async with async_session() as session:
+        user = await session.scalar(select(User).where(User.telegram_id == telegram_id))
+        
+        if not user:
+            user = User(telegram_id=telegram_id)
+            session.add(user)
+            await session.commit()
+            await session.refresh(user)
+        
+        return user
 
