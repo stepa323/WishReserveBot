b7de3e8696e886f8d0a5c29f81c2ab7ce71aa387
stepan <voytov.st.vi@gmail.com>
2025-08-10 13:42:36 +0200

    addding db in config.py

--- config/config.py
+++ config/config.py
@@ -1,25 +1,35 @@
 from dataclasses import dataclass
 from environs import Env
+from typing import List, Optional
 
+@dataclass
+class DbConfig:
+    url: str
+    echo: bool = False  # SQL query logging
 
 @dataclass
 class TgBot:
     token: str
-    admin_ids: list[int]
-
+    admin_ids: List[int]
+    use_redis: bool = False
 
 @dataclass
 class Config:
     bot: TgBot
+    db: DbConfig
 
-
-def load_config(path: str | None = None) -> Config:
-    env: Env = Env()
+def load_config(path: Optional[str] = None) -> Config:
+    env = Env()
     env.read_env(path, override=True)
 
     return Config(
         bot=TgBot(
-            token=env('BOT_TOKEN'),
-            admin_ids=list(map(int, env.list('ADMIN_IDS')))
+            token=env.str("BOT_TOKEN"),
+            admin_ids=list(map(int, env.list("ADMIN_IDS"))),
+            use_redis=env.bool("USE_REDIS", False)
+        ),
+        db=DbConfig(
+            url=env.str("DB_URL", "sqlite+aiosqlite:///database.db"),
+            echo=env.bool("DB_ECHO", False)
         )
     )
