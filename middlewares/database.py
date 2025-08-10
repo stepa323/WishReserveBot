4a2b42238ad1a567836af90a983319eb3010081c
stepan <voytov.st.vi@gmail.com>
2025-08-10 15:23:04 +0200

    Update database.py

--- middlewares/database.py
+++ middlewares/database.py
@@ -6,17 +6,7 @@ from database.db import Database
 from sqlalchemy.ext.asyncio import AsyncSession
 
 class DatabaseMiddleware(BaseMiddleware):
-    """Middleware that injects database session into handlers"""
-    def __init__(self, db_conn: DatabaseConnection):
-        super().__init__()
-        self.db_conn = db_conn
-
-    async def __call__(
-        self,
-        handler: Callable[[TelegramObject, dict[str, Any]], Awaitable[Any]],
-        event: TelegramObject,
-        data: dict[str, Any]
-    ) -> Any:
-        async with self.db_conn.get_session() as session:
-            data['db'] = Database(session)
+    async def __call__(self, handler, event, data):
+        async with self.db_conn.SessionLocal() as session:
+            data['db_session'] = session
             return await handler(event, data)
