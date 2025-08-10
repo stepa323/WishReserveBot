9569dd8206da21be256205e91394b76b75026c12
stepan <voytov.st.vi@gmail.com>
2025-08-10 12:44:14 +0200

    Update user.py: added answering to callback

--- handlers/user.py
+++ handlers/user.py
@@ -20,6 +20,9 @@ async def process_start_message(message: Message, i18n: dict[str, str]):
 @router.callback_query(F.data == 'start_menu', StateFilter(default_state))
 async def process_start_message(callback: CallbackQuery, i18n: dict[str, str]):
     keyboard = create_inline_kb(2, i18n, 'btn_my_wishlists', 'btn_friends_wishlists', 'btn_help')
+
+    await callback.answer()
+    
     await callback.message.edit_text(
         text=i18n.get('/start'),
         reply_markup=keyboard
@@ -31,6 +34,8 @@ async def process_btn_my_wishlist_click(callback: CallbackQuery, i18n: dict[str,
     # Получаем списки желаний из базы данных
     wishlists = False
 
+    await callback.answer()
+    
     keyboard = create_inline_kb(1, i18n, 'btn_create_wishlist', start_menu='btn_go_back')
     if not wishlists:
         await callback.message.edit_text(
@@ -49,6 +54,8 @@ async def process_btn_friends_wishlists(callback: CallbackQuery, i18n: dict[str,
     # Получаем списки желаний друзей из базы данных
     wishlists = False
 
+    await callback.answer()
+    
     keyboard = create_inline_kb(1, i18n, 'btn_create_wishlist', start_menu='btn_go_back')
     if not wishlists:
         await callback.message.edit_text(
@@ -65,6 +72,9 @@ async def process_btn_friends_wishlists(callback: CallbackQuery, i18n: dict[str,
 @router.callback_query(F.data == 'btn_help', StateFilter(default_state))
 async def process_btn_help(callback: CallbackQuery, i18n: dict[str, str]):
     keyboard = create_inline_kb(1, i18n, 'btn_support', start_menu='btn_go_back')
+    
+    await callback.answer()
+    
     await callback.message.edit_text(
         text=i18n.get('/help'),
         reply_markup=keyboard
@@ -74,6 +84,9 @@ async def process_btn_help(callback: CallbackQuery, i18n: dict[str, str]):
 @router.message(Command(commands='/help'), StateFilter(default_state))
 async def process_btn_help(message: Message, i18n: dict[str, str]):
     keyboard = create_inline_kb(1, i18n, 'btn_support')
+
+    await callback.answer()
+    
     await message.answer(
         text=i18n.get('/help'),
         reply_markup=keyboard
